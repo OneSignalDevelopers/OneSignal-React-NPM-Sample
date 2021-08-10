0119bb5347af551692c053f7c41f679a0732aeda
Patricio Vargas <pato@onesignal.com>
2021-08-10 16:02:32 -0400

    advanced setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  window.OneSignal = window.OneSignal || [];
+  const OneSignal = window.OneSignal;
+
+  useEffect(() => {
+    OneSignal.push(() => {
+      OneSignal.init(
+        {
+          appId: "68782627-f22d-45b7-b1eb-44b826777b50"
+        }
+      )
+    });
+  }, [OneSignal]);
+
   return (
     <div className="App">
       
