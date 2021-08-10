c330b749d0d9165b494a80446059c14c7844a9b2
Patricio Vargas <pato@onesignal.com>
2021-08-10 16:54:46 -0400

    logo added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import OneSignal from 'react-onesignal';
+import logo from './logo.png'
 
 
 function App() {
@@ -20,9 +21,12 @@ function App() {
 
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="tags">
         <button className="btn btn--react" onClick={()=>onHandleTag('react')}>ReactJS</button>
         <button className="btn btn--angular" onClick={()=>onHandleTag('angular')}>Angular</button>
         <button className="btn btn--vue" onClick={()=>onHandleTag('vue')}>Vue</button>
+      </div>
     </div>
   );
 }
