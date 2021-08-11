5943058365f11605e8e2ac023ca7de49f49dce03
Patricio Vargas <pato@onesignal.com>
2021-08-11 11:51:58 -0400

    demo start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import { useEffect } from 'react';
 import './App.css';
 import logo from './logo.png'
 
 function App() {
-  window.OneSignal = window.OneSignal || [];
-  const OneSignal = window.OneSignal;
-
-  useEffect(() => {
-    OneSignal.push(() => {
-      OneSignal.init(
-        {
-          appId: "68782627-f22d-45b7-b1eb-44b826777b50"
-        }
-      )
-    });
-  }, [OneSignal]);
 
   const onHandleTag = (tag) => {
     console.log('Tagging');
-    OneSignal.sendTag("tech", tag).then(() => {
-      console.log("Sent tag: " + tag);
-    });
   }
 
   return (
